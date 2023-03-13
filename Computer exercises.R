cf72ff57d27f751999051dcae0dc38760edc7921
carlo-varri <121613137+carlo-varri@users.noreply.github.com>
2023-03-13 23:09:43 +0000

    Chapter three exercises

--- Computer exercises.R
+++ Computer exercises.R
@@ -1,2 +1,85 @@
 install.packages("wooldridge")
 library(wooldridge)
+library(fixest)
+library(tidyverse)
+#Chapter 3 exercises 
+
+####3.13####
+#iii
+lm_with_faminc <- feols(bwght ~ cigs + faminc, data = bwght)
+lm_without_faminc <- feols(bwght ~ cigs, data = bwght)
+etable(lm_with_faminc, lm_without_faminc)
+#adding faminc doesn't have much of an impact on the estimated effects of 
+#cigs - highly significant both times 
+#This is due to the fact that cigs and faminc are not very correlated ( -0.1730449), and 
+#the coefficient on faminc is practically small.
+
+####3.14####
+hprice1_regression <- lm(price ~ sqrft + bdrms, data = hprice1)
+summary(hprice1_regression)
+
+####3.15####
+#i
+ceo_sal_regression <- lm(log(salary) ~ log(sales) + log(mktval), data = ceosal2)
+summary(ceo_sal_regression)
+#ii
+ceo_sal_regression_with_profits <- lm(log(salary) ~ log(sales) + log(mktval) + profits, data = ceosal2)
+summary(ceo_sal_regression_with_profits)
+
+#R2 of only 0.299 - not most of the variation
+
+#iii
+ceo_sal_regression_with_ceoten <- lm(log(salary) ~ log(sales) + log(mktval) + profits + ceoten, data = ceosal2)
+summary(ceo_sal_regression_with_ceoten)
+#another year increases salary by 1.2%
+
+#iv
+cor(log(ceosal2$mktval), ceosal2$profits)
+#yes, highly correlated. Not surprising profits is not significant 
+
+####3.16#####
+#i
+fn <- function(i) {
+  print(paste("min =", min(i)))
+  print(paste("max =", max(i)))
+  print(paste("av = ", mean(i)))
+}
+
+fn(attend$atndrte)
+fn(attend$priGPA)
+fn(attend$ACT)
+
+#ii
+attend_regression <- lm(atndrte ~ priGPA + ACT, data = attend)
+summary(attend_regression)
+
+#iii
+#surprising that the sign on ACT is negative. One more ACT point reduces 
+#attendance rate by 1.7 pp
+
+####3.17#### 
+#skipped - will come back to it 
+
+#### 3.18 ####
+
+#i
+iq_on_educ <- lm(IQ ~ educ, data = wage2)
+summary(iq_on_educ)
+deltahat <- iq_on_educ$coefficients[[2]]
+
+#ii
+wage_on_educ <- lm(log(wage) ~ educ, data = wage2)
+summary(wage_on_educ)
+B1bar <- wage_on_educ$coefficients[[2]]
+
+#iii
+mlr <-  lm(log(wage) ~ educ + IQ, data = wage2)
+B1hat <- mlr$coefficients[[2]]
+B2hat <- mlr$coefficients[[3]]
+
+B1hat + B2hat*deltahat == B1bar
+
+
+
+
+                 
